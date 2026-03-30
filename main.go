2e9897c1f1304955b0d48bce88c279275b287fc5
nicolas11alex <rodriguezvillotanicolas@gmail.com>
2026-03-30 14:04:46 -0500

    cambio de nombre

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Nicolas"
+	dinero := 1000000
+	carro := "Mustang GT"
+	fmt.Println(nombre ,"quiere comprar un",carro,"y tiene",dinero)
+
+}
