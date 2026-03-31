e5d17c10a58e4facd592b76e78d17cc1a3d4a638
nicolas11alex <rodriguezvillotanicolas@gmail.com>
2026-03-31 14:59:47 -0500

    prueba función

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import "fmt"
+func puedeComprar(dineroAhorrado, precio int)bool{
+	return dineroAhorrado >= precio
+}
 
 func main() {
 	nombre := "Nicolas"
-	dinero := 1000000
-	carro := "Mustang GT"
-	fmt.Println(nombre ,"quiere comprar un",carro,"y tiene",dinero)
+	dineroAhorrado := 3000000
+	carros := []string{"Mustang", "Camaro", "Raptor"}
+	precios := []int{2000000, 100000, 2500000}
+
+	for i,carro := range carros{
+		precio := precios[i]
+		if puedeComprar(dineroAhorrado,precio){
+			fmt.Printf("Felicidades %s tu tienes $%d y puedes comprar un %s por que cuesta $%d\n", nombre, dineroAhorrado, carro, precio)
+		}else {
+			fmt.Printf("%s NO puedes comprar el %s ($%d)\n", nombre, carro, precio)
+		}
+	}
 
 }
