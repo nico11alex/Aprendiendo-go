6cd2e8cc270270da62ac429dc247a791c9a81af8
nicolas11alex <rodriguezvillotanicolas@gmail.com>
2026-04-11 16:15:11 -0500

    API

--- main.go
+++ main.go
@@ -1,66 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-type Persona struct {
-	Nombre string
-	Edad   int
-	Dinero int
+func saludo(w http.ResponseWriter, r *http.Request) {
+
+	nombre := r.URL.Query().Get("nombre")
+
+	if nombre == "" {
+		nombre = "Invitado"
+	}
+
+	fmt.Fprintf(w, "Hola %s bienvenido a mi primera API en go\n", nombre)
+	fmt.Fprintf(w, "El metodo request utilizado es %s\n", r.Method)
+	fmt.Fprintf(w, "La ruta utilizada es %s\n", r.URL)
 }
-
-func esMayorDeEdad(edad int)bool{
-	return edad >=18
-}
-
-func puedeComprar(dinero,precio int)bool{
-	return dinero >= precio
-}
-
 
 func main() {
 
-	var nombre string
-	var edad int
-	var dinero int
-
-	fmt.Println("Escriba su nombre: ")
-	fmt.Scan(&nombre)
-
-	fmt.Println("Escriba su edad: ")
-	_, err := fmt.Scanln(&edad)
-
-	if err != nil {
-		fmt.Println("Error: ingresa un número válido")
-		return
-	}
-
-	fmt.Println("Escriba su dinero ahorrado: ")
-	_,err = fmt.Scanln(&dinero)
-
-	if err != nil {
-		fmt.Println("Error: ingresa un número válido")
-		return
-	}
-
-	precio:=2000000
-
-	personas := Persona{
-		Nombre: nombre,
-		Edad: edad,
-		Dinero: dinero,
-	}
-
-	if !esMayorDeEdad(personas.Edad){
-		fmt.Printf("%s perdon pero no puedes comprar un carro porque tienes %d años y eres menor de edad\n",personas.Nombre,personas.Edad)
-		return
-	}
-
-	if !puedeComprar(personas.Dinero,precio){
-		fmt.Printf("%s perdon pero con tu dinero no te alcanza el carro\n",personas.Nombre)
-		return
-	}
-	
-	fmt.Printf("%s felicidades puedes comprar tu Mustang de 2000000",personas.Nombre)
-
+	http.HandleFunc("/saludo", saludo)
+	fmt.Println("Servidor en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 
 }
