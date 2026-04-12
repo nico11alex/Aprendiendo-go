9acac59583e69bf1c17104b1507b0a18b3b93953
nicolas11alex <rodriguezvillotanicolas@gmail.com>
2026-04-11 20:21:21 -0500

    ejercicio de structs

--- main.go
+++ main.go
@@ -1,45 +1,120 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
-type Persona struct {
-	Nombre string `json:"nombre"`
-	Edad   int    `json:"edad"`
-}
+import "fmt"
 
 type Carro struct {
-	Nombre string `json:"nombre"`
-	Precio int `json:"precio"`
+	Nombre string
+	Modelo int
+	Precio int
 }
 
-func obtenerPersona(w http.ResponseWriter, r *http.Request) {
-
-	personas := []Persona{
-		{"Nicolas", 20},
-		{"Sara", 22},
-		{"Jesus", 15},
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(personas)
+type Persona struct {
+	Nombre string
+	Edad   int
+	Sueldo int
 }
 
-func obtenerCarros(w http.ResponseWriter, r *http.Request){
-
-	carros :=[]Carro{
-		{"Mazda", 114350000},
-		{"Mustang gt", 279990000},
+func listaDeCarros(carros []Carro) {
+	fmt.Println("Nuestros carros")
+	for _, carro := range carros {
+		fmt.Printf("%s ", carro.Nombre)
 	}
+	fmt.Println("")
+	fmt.Printf("Escoja un carro:")
+}
 
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(carros)
+func validacionEdad(persona Persona)bool{
+	if persona.Edad < 18{
+		return false
+	}
+	return true
+}
+
+func calcularCuantoTiempoFaltaEdad(persona Persona)int{
+	tiempoFaltante := 18 - persona.Edad
+	return tiempoFaltante
+}
+
+func confirmarCantidadDinero(nombreCarro,nombrePersona string, modelo, precio,sueldo int)bool{
+	if(sueldo>=precio){
+		fmt.Printf("Felicidades %s puedes comprar un %s modelo %d con tu sueldo",nombrePersona,nombreCarro,modelo)
+		return true
+	}
+	return false
+}
+
+func calcularCuantoTiempoFaltaDinero(precio,sueldo int)int{
+	meses := 0
+	ahorro:=0
+	for ahorro <= precio {
+		ahorro += sueldo
+		meses ++
+	}
+	return meses
 }
 
 func main() {
-	http.HandleFunc("/persona", obtenerPersona)
-	http.HandleFunc("/carros",obtenerCarros)
-	http.ListenAndServe(":8080", nil)
+
+	var carroElejido string
+	var nombre string
+	var edad int
+	var sueldo int
+
+	fmt.Printf("Cual es tu nombre: ")
+	_, err := fmt.Scanln(&nombre)
+	if err != nil {
+		fmt.Println("Tu nombre tiene un formato incorrecto")
+		return
+	}
+
+	fmt.Printf("Cual es tu edad: ")
+	_, err = fmt.Scanln(&edad)
+	if err != nil {
+		fmt.Println("Tu edad no puede ser guardada revisala")
+		return
+	}
+
+	fmt.Printf("Cual es tu sueldo: ")
+	_, err = fmt.Scanln(&sueldo)
+	if err != nil {
+		fmt.Println("Tu sueldo no puede ser guardada revisala")
+		return
+	}
+
+	persona := Persona{
+		nombre,
+		edad,
+		sueldo,
+	}
+
+	carros := []Carro{
+		{"Mustang", 2020, 200000000},
+		{"Mazda", 2021, 15000000},
+	}
+
+	if !validacionEdad(persona){
+		fmt.Printf("Lo siento %s pero aun te faltan %d años para tratar de comprar un carro",persona.Nombre,calcularCuantoTiempoFaltaEdad(persona))
+		return
+	}
+
+	listaDeCarros(carros)
+
+	_, err = fmt.Scanln(&carroElejido)
+	if err != nil {
+		fmt.Println("Eso no es un carro")
+		return
+	}
+
+	for _, carro := range carros {
+		if carro.Nombre == carroElejido {
+			if confirmarCantidadDinero(carro.Nombre,persona.Nombre,carro.Modelo,carro.Precio,persona.Sueldo){
+				return
+			}
+
+			fmt.Printf("Perdon %s aun te falta %d meses para comprar el %s que quieres",persona.Nombre,calcularCuantoTiempoFaltaDinero(carro.Precio,sueldo),carro.Nombre)
+			return
+		}
+	}
+
+	fmt.Println("Carro no encontrado")
 }
