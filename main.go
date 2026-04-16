d1a838f17f8eeed8e98c2827fb30f6457344975b
nicolas11alex <rodriguezvillotanicolas@gmail.com>
2026-04-15 23:25:17 -0500

    creacion de errores

--- main.go
+++ main.go
@@ -1,87 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-type Persona struct{
-	Nombre string
-	Edad int
-	Pais string
-	Altura float64
+type Persona struct {
+	Nombre       string
+	Edad         int
+	Pais         string
+	Altura       float64
 	EsEstudiante bool
-	Hobbies []string
+	Hobbies      []string
 }
 
-const ( 
-		mayorDeEdad           = 18
-		edadMinimaAdolescente = 13 
-	)
-
-func imprimirInfoPersonal(nombre string ,edad int,pais string,altura float64,esEstudiante bool){
-	fmt.Println("\n=== Datos Técnicos ===")
-	fmt.Printf("Nombre:      %s\n", nombre)
-	fmt.Printf("Edad:        %d años\n", edad)
-	fmt.Printf("País:        %s\n", pais)
-	fmt.Printf("Altura:      %.2f m\n", altura)
-	fmt.Printf("Estudiante:  %t\n", esEstudiante)
-	fmt.Println("============================")
-}
-
-func determinarCategoriaEdad(edad,mayorDeEdad,edadMinimaAdolescente int)string{
-	switch{
+func determinarCategoriaEdad(edad int) (string, error) {
+	switch {
 	case edad < 0 || edad > 130:
-		return "Tu edad no puede ser cierta"
-	case edad >= mayorDeEdad:
-		return  "Eres mayor de edad"
-	case edad >= edadMinimaAdolescente:
-		return  "Eres adolescente"
+		return "", errors.New("edad fuera de rango válido")
+	case edad >= 18:
+		return "Eres mayor de edad", nil
+	case edad >= 13:
+		return "Eres adolescente", nil
 	default:
-		return "Eres un niño"
+		return "Eres un niño", nil
 	}
 
 }
 
-func contarNumerosPares()int{
-	contador :=0
+func contarNumerosPares() int {
+	contador := 0
 	for i := 1; i <= 10; i++ {
-		if i%2 == 0{
-			fmt.Printf("El número %d es par\n",i)
-			contador ++
-		}else{
-			fmt.Printf("El número %d es impar\n",i)
+		if i%2 == 0 {
+			fmt.Printf("El número %d es par\n", i)
+			contador++
+		} else {
+			fmt.Printf("El número %d es impar\n", i)
 		}
 	}
 
 	return contador
 }
 
-func imprimirHobbies(hobbies []string){
+func imprimirHobbies(hobbies []string) {
 	fmt.Println("Tus hobbies son:")
-	for indice,hobby := range hobbies{
-		fmt.Printf("Tu hobby número %d es %s\n",indice+1, hobby)
+	for indice, hobby := range hobbies {
+		fmt.Printf("Tu hobby número %d es %s\n", indice+1, hobby)
 	}
 }
 
-func agregarHobby(hobbies []string, nuevoHobby string) []string{
+func agregarHobby(hobbies []string, nuevoHobby string) []string {
 	return append(hobbies, nuevoHobby)
 }
 
-func nuevaPersona(nombre, pais string, edad int, altura float64, esEstudiante bool, hobbies []string) Persona{
+func nuevaPersona(nombre, pais string, edad int, altura float64, esEstudiante bool, hobbies []string) Persona {
 	persona := Persona{
-		Nombre: nombre,
-		Edad: edad,
-		Pais: pais,
-		Altura: altura,
+		Nombre:       nombre,
+		Edad:         edad,
+		Pais:         pais,
+		Altura:       altura,
 		EsEstudiante: esEstudiante,
-		Hobbies: hobbies,
+		Hobbies:      hobbies,
 	}
 
 	return persona
 }
 
-func imprimirPersona(persona Persona){
+func imprimirPersona(persona Persona) {
 	fmt.Println("\n=== Datos Técnicos ===")
 	fmt.Printf("Nombre:      %s\n", persona.Nombre)
 	fmt.Printf("Edad:        %d años\n", persona.Edad)
+	resultado,err := determinarCategoriaEdad(persona.Edad)
+	if err != nil{
+		fmt.Println("Error :",err)
+		os.Exit(1)
+	}
+	fmt.Printf("Categoria:   %s\n",resultado)
 	fmt.Printf("País:        %s\n", persona.Pais)
 	fmt.Printf("Altura:      %.2f m\n", persona.Altura)
 	fmt.Printf("Estudiante:  %t\n", persona.EsEstudiante)
@@ -91,25 +86,22 @@ func imprimirPersona(persona Persona){
 
 func main() {
 
-	hobbies:=[]string {"Programar","Leer","Baloncesto","Video juegos","Escribir"}
+	hobbies := []string{"Programar", "Leer", "Baloncesto", "Video juegos", "Escribir"}
 
 	nombre := "Nicolas"
 	edad := 18
 	pais := "Colombia"
 	altura := 1.80
 	esEstudiante := true
-	
-	hobbies = agregarHobby(hobbies,"Comer")
 
-	yo := nuevaPersona(nombre,pais,edad,altura,esEstudiante,hobbies)
+	hobbies = agregarHobby(hobbies, "Comer")
+
+	yo := nuevaPersona(nombre, pais, edad, altura, esEstudiante, hobbies)
 	imprimirPersona(yo)
 
 	fmt.Println("")
-	fmt.Println(determinarCategoriaEdad(edad,mayorDeEdad,edadMinimaAdolescente))
-	fmt.Println("")
-
 	fmt.Println("Números del 1 al 10")
-	fmt.Printf("Hemos encontrado %d números pares\n",contarNumerosPares())
+	fmt.Printf("Hemos encontrado %d números pares\n", contarNumerosPares())
 	fmt.Println("")
 	fmt.Println("Estoy aprendiendo Go en Windows")
 }
