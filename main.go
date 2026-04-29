d9adcb7ef8051a501e89573952a9aad4f3e6408d
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-04-28 23:03:42 -0500

    Interfaces más a profundidad

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import "fmt"
 
+type Figura interface{
+	Area() int
+}
+
 type Rectangulo struct{
 	Ancho int
 	Alto int
 }
 
+type Cuadrado struct{
+	Lado int
+}
+
+func ImprimirInformacion(f Figura) {
+    fmt.Printf("El área de esta figura es: %d\n", f.Area())
+}
+
 func(r Rectangulo) Area()int{
 	return r.Alto*r.Ancho
+}
+
+func(c Cuadrado) Area()int{
+	return c.Lado*c.Lado
 }
 
 func(r *Rectangulo) Escalar(factor int){
@@ -20,13 +36,36 @@ func(r *Rectangulo) Escalar(factor int){
 
 }
 
+func(c *Cuadrado) Escalar(factor int){
+	if(factor <=0){
+		return
+	}
+	c.Lado *= factor
+
+}
+
 func main() {
 	rectangulo := Rectangulo{
 		Ancho: 4,
 		Alto: 3,
 	}
+	cuadrado := Cuadrado{
+		Lado: 5,
+	}
+
+	// rectangulo
 
 	fmt.Printf("El area del rectangulo es de %d\n",rectangulo.Area())
 	rectangulo.Escalar(2)
 	fmt.Printf("El area del rectangulo es de %d\n",rectangulo.Area())
+
+	// cuadrado
+
+	fmt.Printf("El area del cuadrado es de %d\n",cuadrado.Area())
+	cuadrado.Escalar(2)
+	fmt.Printf("El area del cuadrado es de %d\n",cuadrado.Area())
+
+	// Figura
+	ImprimirInformacion(rectangulo)
+	ImprimirInformacion(cuadrado)
 }
