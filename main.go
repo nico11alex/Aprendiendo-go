9ad593fe55a5bde67a637bc02d0de4e9f770b3f0
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-05-03 23:24:33 -0500

    patron producto-consumidor

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func echo(c chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for s := range c{
-		fmt.Printf("Eco: %s\n",s)
+	for s := range c {
+		fmt.Printf("Eco: %s\n", s)
 	}
 }
 
@@ -16,14 +16,15 @@ func main() {
 	c := make(chan string)
 	var wg sync.WaitGroup
 
-	wg.Add(1)
+	wg.Add(2)
 	go echo(c, &wg)
-
-	enviar := []string{"Hola", "Mundo", "Go"}
-	for _,s := range enviar{
-		c <- s	
+	go echo(c, &wg)
+	enviar := []string{"1", "2"}
+	for i := 0; i <= 10; i++ {
+		for _, s := range enviar {
+			c <-s
+		}
 	}
-	
 	close(c)
 	wg.Wait()
 	fmt.Println("Terminado.")
