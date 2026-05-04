c8d8f14663d93361bc782f9a2a0595feea1c82e9
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-05-03 22:26:43 -0500

    chanels

--- go.mod
+++
@@ -1,3 +0,0 @@
-module aprendiendo-go
-
-go 1.26.1
--- main.go
+++ main.go
@@ -1,54 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func concesionario1(name string, wg *sync.WaitGroup, resultados chan<- string) {
-    defer wg.Done()
-
-    mensaje := fmt.Sprintf("%s tu mustang cuesta $279.990.000 COP en el consecionario Autoland Colombia",name)
-
-    resultados <- mensaje
+func generarNumeros(n int, c chan int) {
+	for i := 1; i <= n; i++ {
+		c <- i
+	}
+	close(c)
 }
 
-func concesionario2(name string, wg *sync.WaitGroup, resultados chan<- string) {
-    defer wg.Done()
-
-    mensaje := fmt.Sprintf("%s tu mustang cuesta $249.900.000 COP en el consecionario El Roble Motor (Pereira/Medellín/Cali):.",name)
-
-    resultados <- mensaje
-}
-
-func concesionario3(name string, wg *sync.WaitGroup, resultados chan<- string) {
-    defer wg.Done()
-
-    mensaje := fmt.Sprintf("%s tu mustang cuesta $364.900.000 COP en el consecionario Los Coches",name)
-
-    resultados <- mensaje
-}
+func main() {
+	c := make(chan int)
+	go generarNumeros(5, c)
 
 
-func main(){
-    var wg sync.WaitGroup
-    
-    resultados := make(chan string, 3)
-
-    
-    wg.Add(1)
-    go concesionario1("Nicolas",&wg,resultados)
-    wg.Add(1)
-    go concesionario2("Nicolas",&wg,resultados)
-    wg.Add(1)
-    go concesionario3("Nicolas",&wg,resultados)
-    
-    wg.Wait()
-    close(resultados)
-
-    fmt.Println("\nResultados recibidos:")
-    for mesage := range resultados {
-        fmt.Println("→", mesage)
-    }
-    
+	for i := range c{
+		fmt.Println(i)
+	}
+	fmt.Println("Terminado.")
 }
