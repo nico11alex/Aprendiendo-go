e8c41422980a66e5e61caa048de0f79dee3f05e1
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-05-06 23:26:21 -0500

    ejercicio de select

--- main.go
+++ main.go
@@ -1,52 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
-type Comprable interface {
-	Precio() int
-}
-
-type Mazda struct {
-	Modelo int
-	Valor  int
-}
-
-type ErrorPresupuesto struct{
-	Diferencia int
-}
-
-func (m Mazda) Precio() int {
-	return m.Valor
-}
-
-func RealizarCompra(c Comprable, presupuesto int,e *ErrorPresupuesto) (string, error) {
-	if c.Precio() > presupuesto {
-		Diferencia(c,presupuesto,e) 
-		MensajeError := fmt.Sprintf("Te faltan solo %d meses de ahorro si guardas 2 millones al mes.", e.Diferencia)
-		err := errors.New(MensajeError)
-		return "", err
-	} else {
-		return "Felicidades puedes comprar el auto", nil
-	}
-}
-
-func Diferencia(c Comprable, presupuesto int, e *ErrorPresupuesto){
-	for c.Precio() > presupuesto {
-		presupuesto += 2000000
-		e.Diferencia ++
-	}
+func buscarRespuesta(resultado chan<- string) {
+	time.Sleep(3 * time.Second)
+	resultado <- "Datos encontrados"
+	close(resultado)
 }
 
 func main() {
-	mazda := Mazda{Modelo: 2026, Valor: 140000000}
-	errores := ErrorPresupuesto{Diferencia: 0}
-	mensaje, errorCompra := RealizarCompra(mazda, 13000000,&errores)
-	if errorCompra != nil{
-		fmt.Println(errorCompra)
-	}else{
-		fmt.Println(mensaje)
+	c := make(chan string)
+	go buscarRespuesta(c)
+
+	select{
+	case msg := <-c :
+		fmt.Printf("Éxito: %s\n",msg)
+	case <-time.After(2*time.Second):
+		fmt.Println("Tiempo agotado. La búsqueda tardó demasiado.")
 	}
+	fmt.Println("Programa finalizado.")
 }
