c2866ad2b78d5ef11eb58cccf867b8b300ffa346
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-05-18 23:37:50 -0500

    error con go routines

--- main.go
+++ main.go
@@ -1,146 +1,67 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"sync"
+	"time"
 )
 
-type ErrorRegistro struct {
-	Codigo  int
-	Campo   string
-	Mensaje string
+type TareaResultado struct {
+	ID        int
+	Tarea     string
+	Resultado string
+	Error     error
+	Tiempo    time.Duration
 }
 
-func (e ErrorRegistro) Error() string {
-	return fmt.Sprintf("Error en el campo %s: %s", e.Campo, e.Mensaje)
-}
+func procesarTarea(id int, c chan<- TareaResultado){
+	start := time.Now()
+	tareaNombre := fmt.Sprintf("Tarea-%d", id)
 
-func registrarUsuario(nombre string, edad int, email string, password string) (string, error) {
-	var errs []error
-	var err error
-	if err = validarNombre(nombre); err != nil {
-		errs = append(errs, err)
-	}
-	if err = validarEdad(edad); err != nil {
-		errs = append(errs, err)
-	}
-	if err = validarEmail(email); err != nil {
-		errs = append(errs, err)
-	}
+	// Simular trabajo
+	time.Sleep(time.Duration(id) * 300 * time.Millisecond)
 
-	if err = validarPassword(password); err != nil {
-		errs = append(errs, err)
-	}
-
-	if len(errs) > 0 {
-		return "", errors.Join(errs...)
-	}
-
-	return "Todo correcto", nil
-
-}
-
-func validarNombre(nombre string) error {
-	if len(nombre) < 3 {
-		return ErrorRegistro{
-			Codigo:  1,
-			Campo:   "nombre",
-			Mensaje: "El nombre es muy corto",
+	// Algunas tareas fallan
+	if id == 2 || id == 4 {
+		c <-TareaResultado{
+			ID:     id,
+			Tarea:  tareaNombre,
+			Error:  fmt.Errorf("fallo en la conexión con el servidor"),
+			Tiempo: time.Since(start),
 		}
-	}
-	return nil
-}
-
-func validarEdad(edad int) error {
-	if edad < 18 || edad > 100 {
-		return ErrorRegistro{
-			Codigo:  2,
-			Campo:   "edad",
-			Mensaje: "No estas en el rango de edad",
-		}
-	}
-	return nil
-}
-
-func validarEmail(email string) error {
-	for _, char := range email {
-		if char == '@' {
-			return nil
-		}
-	}
-	return ErrorRegistro{
-		Codigo:  3,
-		Campo:   "email",
-		Mensaje: "no tiene un formato correcto",
-	}
-}
-
-func validarPassword(password string) error {
-	var errs []error
-	var err error
-	var letraMayuscula bool
-	var numero bool
-	if len(password) > 0 {
-		err = ErrorRegistro{
-			Codigo:  4,
-			Campo:   "contraseña",
-			Mensaje: "muy corta",
-		}
-		errs = append(errs, err)
-	}
-	for _, char := range password {
-		if char >= 'A' && char <= 'Z' {
-			letraMayuscula = true
-		}
-		if char >= '0' && char <= '9' {
-			numero = true
-		}
-	}
-	if !letraMayuscula {
-		err = ErrorRegistro{
-			Codigo:  5,
-			Campo:   "contraseña",
-			Mensaje: "Le falta una letra mayuscula",
-		}
-		errs = append(errs, err)
-	}
-	if !numero {
-		err = ErrorRegistro{
-			Codigo:  6,
-			Campo:   "contraseña",
-			Mensaje: "Le falta un número",
-		}
-		errs = append(errs, err)
+		return
 	}
 
-	if len(errs) > 0 {
-		return errors.Join(errs...)
+	c <-TareaResultado{
+		ID:        id,
+		Tarea:     tareaNombre,
+		Resultado: "Procesado correctamente",
+		Tiempo:    time.Since(start),
 	}
-	return nil
 }
 
 func main() {
-	pruebas := []struct {
-		nombre   string
-		edad     int
-		email    string
-		password string
-	}{
-		{"Nicolas", 25, "nicolas@gmail.com", "12234234"}, // éxito
-		{"Ni", 20, "nicolasgmail.com", "Nicolas12"},      // nombre corto + sin @
-		{"Carlos", 15, "carlos@ok.com", "sadsadsajdk23"}, // edad inválida
-		{"Ana", 5, "ana", "as"},                          // múltiples errores
+	var wg sync.WaitGroup
+	c := make(chan TareaResultado)
+
+	for i:=1;i <= 5;i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			procesarTarea(id,c)
+		}(i)
 	}
 
-	for _, p := range pruebas {
-		fmt.Printf("Registrando: %s (%d) - %s, contraseña: %s\n", p.nombre, p.edad, p.email, p.password)
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
 
-		_, err := registrarUsuario(p.nombre, p.edad, p.email, p.password)
-
-		if err != nil {
-			fmt.Printf("❌ Errores: %v\n\n", err)
-		} else {
-			fmt.Println("✅ Registro exitoso")
+	for  res := range c{
+		if res.Error != nil{
+			fmt.Printf("Error en la %s: %v\n",res.Tarea,res.Error)
+		}else{
+			fmt.Printf("La %s se %s con un tiempo de %v\n",res.Tarea,res.Resultado,res.Tiempo)
 		}
 	}
 }
