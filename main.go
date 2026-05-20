c2292afefe376fd7871804dc7786da455000f906
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-05-19 23:23:44 -0500

    clean code con structs

--- main.go
+++ main.go
@@ -5,44 +5,88 @@ import (
 )
 
 type ErrorUserValidation struct {
-	Campo  string
+	Campo   string
 	Mensaje string
+}
+
+type user struct {
+	generalInformation generalInformation
+	securityData       securityData
+	actionsUser        actionsUser
+}
+
+type generalInformation struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+type securityData struct {
+	password string
+}
+
+type actionsUser struct {
+	active bool
 }
 
 func (e ErrorUserValidation) Error() string {
 	return fmt.Sprintf("error en %s\nproblema: %s", e.Campo, e.Mensaje)
 }
 
-func userValidation(userName string, edad int, password string) (string, error) {
-	if err := validateUserName(userName); err != nil {
-		return "", err
+func (u *user) Active() {
+	u.actionsUser.active = true
+}
+
+func validateData(name string, age int, email string, password string) error {
+	if err := validateUserName(name); err != nil {
+		return err
 	}
-	if err := validateEdad(edad); err != nil {
-		return "", err
+	if err := validateAge(age); err != nil {
+		return err
 	}
 	if err := validatePassword(password); err != nil {
-		return "", err
+		return err
 	}
-	if validateAdmin(userName, password) {
-		return "Bienvenido admin", nil
+	if err := validateEmail(email); err != nil {
+		return  err
 	}
-	return "usuario validado de manera correcto", nil
+
+	return nil
+}
+
+func createUser(name string, age int, email string, password string) (user, error) {
+	if err:=validateData(name,age,email,password);err != nil{
+		return user{},err
+	}
+
+	user := user{
+		generalInformation: generalInformation{
+			Name:  name,
+			Age:   age,
+			Email: email,
+		},
+		securityData: securityData{
+			password: password,
+		},
+	}
+	user.Active()
+	return user, nil
 }
 
 func validateUserName(userName string) error {
 	if len(userName) < 3 {
 		return ErrorUserValidation{
-			Campo:  "nombre de usuario",
+			Campo:   "nombre de usuario",
 			Mensaje: "el nombre es muy corto",
 		}
 	}
 	return nil
 }
 
-func validateEdad(edad int) error {
+func validateAge(edad int) error {
 	if edad < 18 {
 		return ErrorUserValidation{
-			Campo:  "edad",
+			Campo:   "edad",
 			Mensaje: "eres menor de edad y no podemos validar tu usuario",
 		}
 	}
@@ -52,7 +96,7 @@ func validateEdad(edad int) error {
 func validatePassword(password string) error {
 	if len(password) < 8 {
 		return ErrorUserValidation{
-			Campo:  "contraseña",
+			Campo:   "contraseña",
 			Mensaje: "la contraseña es muy corta",
 		}
 	}
@@ -63,11 +107,25 @@ func validateAdmin(userName string, password string) bool {
 	return userName == "admin" && password == "admin123"
 }
 
+func validateEmail(email string) error {
+	for _, char := range email {
+		if char == '@' {
+			return nil
+		}
+	}
+	return ErrorUserValidation{
+		Campo:   "email",
+		Mensaje: "no tiene formato correcto",
+	}
+}
+
 func main() {
-	resultado,err := userValidation("admin",19,"admin1222")
-	if err != nil{
+	resultado, err := createUser("admin", 19, "admin1222@","xskaxksa")
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(resultado)
+	fmt.Println("Usuario creado correctamente")
+	fmt.Printf("Nombre: %s\nEdad: %d\nEmail: %s",resultado.generalInformation.Name,resultado.generalInformation.Age,resultado.generalInformation.Email)
+	
 }
